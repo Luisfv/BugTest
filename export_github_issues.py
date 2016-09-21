c7c726d6c26c4a794f626a96f564e9a225e5bef4
Luis <alternateluis@live.com>
2016-09-20 17:24:09 -0700

    modified code for comments and custom rows

--- export_github_issues.py
+++ export_github_issues.py
@@ -6,11 +6,16 @@ from a repository that username has access to. Supports Github API v3.
 """
 import csv
 import requests
+import sys
 
+if len(sys.argv) != 4:
+    print "Usage: python export_github_issues.py username password username/repo"
+    sys.exit()
+args = sys.argv
 
-GITHUB_USER = ''
-GITHUB_PASSWORD = ''
-REPO = ''  # format is username/repo
+GITHUB_USER = args[1]
+GITHUB_PASSWORD = args[2]
+REPO = args[3]
 ISSUES_FOR_REPO_URL = 'https://api.github.com/repos/%s/issues' % REPO
 AUTH = (GITHUB_USER, GITHUB_PASSWORD)
 
@@ -19,16 +24,33 @@ def write_issues(response):
     if not r.status_code == 200:
         raise Exception(r.status_code)
     for issue in r.json():
-        labels = issue['labels']
-        for label in labels:
-            if label['name'] == "Client Requested":
-                csvout.writerow([issue['number'], issue['title'].encode('utf-8'), issue['body'].encode('utf-8'), issue['created_at'], issue['updated_at']])
+        comments = ""
+        comments_list = []
+        comments_url = issue['comments_url']
+        comments_request = requests.get(comments_url)
+        if not comments_request.status_code == 200:
+            raise Exception(comments_request.status_code)
+        else:
+            comments = comments_request.json()
+        if comments != "":
+            comments_list = get_comments(comments)
 
+        # Select the values from the issue to put into the row
+        row = [issue['title'], issue['body'], comments_list]
+        csvout.writerow(row)
+
+def get_comments(comments):
+    comments_list = []
+    for c in comments:
+        comment = (c['user']['login'].encode('utf-8'), c['body'].encode('utf-8'))
+        comments_list.append(comment)
+    return comments_list
 
 r = requests.get(ISSUES_FOR_REPO_URL, auth=AUTH)
 csvfile = '%s-issues.csv' % (REPO.replace('/', '-'))
 csvout = csv.writer(open(csvfile, 'wb'))
-csvout.writerow(('id', 'Title', 'Body', 'Created At', 'Updated At'))
+# Edit this to change what values to write
+csvout.writerow(('Title', 'Body', 'Comments'))
 write_issues(r)
 
 #more pages? examine the 'link' header returned
