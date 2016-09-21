f45a3e2aca771fcf426df459cfcd8bbd712f0981
Luis <alternateluis@live.com>
2016-09-21 15:50:48 -0700

    fixed getting private repo comments, changed usage for better security

--- export_github_issues.py
+++ export_github_issues.py
@@ -7,27 +7,30 @@ from a repository that username has access to. Supports Github API v3.
 import csv
 import requests
 import sys
+import getpass
 
-if len(sys.argv) != 4:
-    print "Usage: python export_github_issues.py username password username/repo"
+if len(sys.argv) != 3:
+    print "Usage: python export_github_issues.py username username/repo"
     sys.exit()
 args = sys.argv
 
 GITHUB_USER = args[1]
-GITHUB_PASSWORD = args[2]
-REPO = args[3]
+GITHUB_PASSWORD = getpass.getpass()
+REPO = args[2]
 ISSUES_FOR_REPO_URL = 'https://api.github.com/repos/%s/issues' % REPO
 AUTH = (GITHUB_USER, GITHUB_PASSWORD)
 
 def write_issues(response):
     "output a list of issues to csv"
-    if not r.status_code == 200:
-        raise Exception(r.status_code)
-    for issue in r.json():
+    if not response.status_code == 200:
+        raise Exception(response.status_code)
+    for issue in response.json():
+        print issue
         comments = ""
         comments_list = []
-        comments_url = issue['comments_url']
+        comments_url = issue['comments_url'].encode('utf-8') + "?access_token=" + GITHUB_PASSWORD
         comments_request = requests.get(comments_url)
+        print comments_url
         if not comments_request.status_code == 200:
             raise Exception(comments_request.status_code)
         else:
@@ -36,7 +39,7 @@ def write_issues(response):
             comments_list = get_comments(comments)
 
         # Select the values from the issue to put into the row
-        row = [issue['title'], issue['body'], comments_list]
+        row = [issue['title'].encode('utf-8'), issue['body'].encode('utf-8'), comments_list]
         csvout.writerow(row)
 
 def get_comments(comments):
